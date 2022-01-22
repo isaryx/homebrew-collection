720fa02b68c5cf84e5ea84edf833a895a908225d
Vu Tran <isaryx@gmail.com>
2022-01-22 11:35:31 +0700

    fix md5

--- Casks/corretto11.rb
+++ Casks/corretto11.rb
@@ -2,7 +2,7 @@ cask "corretto11" do
     arch = Hardware::CPU.intel? ? "x64" : "aarch64"
   
     version "11.0.14.9.1"
-    md5 "43126576de874ea7c4f9aa3b04dd140a"
+    sha256 "3d76035ea371806eb8e2c809da3f588f30464d5784d0c161d504f69a0bf3446f"
   
     url "https://corretto.aws/downloads/resources/11.0.14.9.1/amazon-corretto-11.0.14.9.1-macosx-x64.pkg"
     name "AWS Corretto JDK"
