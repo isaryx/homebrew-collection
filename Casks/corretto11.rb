60317954f49ec4916a284f47eab8ed339e0e26b1
Vu Tran <isaryx@gmail.com>
2022-01-22 11:40:49 +0700

    corretto11 ~ intel binary

--- Casks/corretto11.rb
+++ Casks/corretto11.rb
@@ -1,15 +1,13 @@
 cask "corretto11" do
-    arch = Hardware::CPU.intel? ? "x64" : "aarch64"
-  
     version "11.0.14.9.1"
     sha256 "3d76035ea371806eb8e2c809da3f588f30464d5784d0c161d504f69a0bf3446f"
   
-    url "https://corretto.aws/downloads/resources/11.0.14.9.1/amazon-corretto-11.0.14.9.1-macosx-x64.pkg"
+    url "https://corretto.aws/downloads/resources/#{version.sub(/-\d+/, "")}/amazon-corretto-#{version}-macosx-x64.pkg"
     name "AWS Corretto JDK"
     desc "OpenJDK distribution from Amazon"
     homepage "https://corretto.aws/"
   
-    pkg "amazon-corretto-11.0.14.9.1-macosx-x64.pkg"
+    pkg "amazon-corretto-#{version}-macosx-x64.pkg"
   
-    uninstall pkgutil: "com.amazon.corretto.11"
+    uninstall pkgutil: "com.amazon.corretto.#{version.major}"
   end
