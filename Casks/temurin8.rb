d4bb4fb372330dcf0a9f4d7547fd01928d736b08
Vu Tran <isaryx@gmail.com>
2022-01-22 12:04:47 +0700

    minor adjust

--- Casks/temurin8.rb
+++ Casks/temurin8.rb
@@ -1,8 +1,9 @@
 cask "temurin8" do
   version '8,312,b07'
+  sha256 "2428e7fbd0dfb7416783df89398c0ccc0fa883f4fabce0e9b83388bc2109268e"
+
   url "https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u#{version.csv[1]}-#{version.csv[2]}/OpenJDK8U-jdk_x64_mac_hotspot_8u#{version.csv[1]}#{version.csv[2]}.pkg",
       verified: "github.com/adoptium/"
-  sha256 "2428e7fbd0dfb7416783df89398c0ccc0fa883f4fabce0e9b83388bc2109268e"
 
   name "Eclipse Temurin Java Development Kit"
   desc "JDK from the Eclipse Foundation (Adoptium)"
