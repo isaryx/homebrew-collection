168e2cea113cd0c0e5f51f630ab41943a3bbbca6
SaRy <isaryx@gmail.com>
2024-12-27 10:37:54 +0700

    Create corretto21.rb

---
+++ Casks/corretto21.rb
@@ -0,0 +1,21 @@
+# https://github.com/corretto/corretto-21
+cask "corretto21" do
+  version "21.0.5.11.1"
+  sha256 "e0673d60b5a7cdd8cfabc646b51cc15c4906beaefd51b96588c0a6de915e5f45"
+
+  url "https://corretto.aws/downloads/resources/#{version.sub(/-\d+/, "")}/amazon-corretto-#{version}-macosx-aarch64.pkg"
+  name "AWS Corretto JDK"
+  desc "OpenJDK distribution from Amazon"
+  homepage "https://github.com/corretto/corretto-11/releases"
+
+  livecheck do
+      url "https://corretto.aws/downloads/latest/amazon-corretto-#{version.major}-aarch64-macos-jdk.pkg"
+      strategy :header_match do |headers|
+        headers["location"][%r{/amazon-corretto-(\d+(?:\.\d+)+)-macosx-aarch64\.pkg}i, 1]
+      end
+    end
+
+  pkg "amazon-corretto-#{version}-macosx-aarch64.pkg"
+
+  uninstall pkgutil: "com.amazon.corretto.#{version.major}"
+end
