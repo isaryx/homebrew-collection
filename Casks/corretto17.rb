9ff6ead4d26603141a1fe4c1359428ba06a48411
SaRy <isaryx@gmail.com>
2025-03-03 08:48:47 +0700

    Update corretto17.rb

--- Casks/corretto17.rb
+++ Casks/corretto17.rb
@@ -1,7 +1,7 @@
 # https://github.com/corretto/corretto-17
 cask "corretto17" do
-  version "17.0.13.11.1"
-  sha256 "dfc929ddc533472052132a88ad292aaf380f5afeb1b39ecaa8a776daecb51d99"
+  version "17.0.14.7.1"
+  sha256 "e168900460d2bb6c887d3a9e90918f2d2568d58e4da17cb208318176c804cc1f"
 
   url "https://corretto.aws/downloads/resources/#{version.sub(/-\d+/, "")}/amazon-corretto-#{version}-macosx-aarch64.pkg"
   name "AWS Corretto JDK"
