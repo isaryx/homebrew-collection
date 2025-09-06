36017fba918081a413efe33c5a036f472f726a70
Vu Tran <Vu.Tran2@netapp.com>
2025-09-06 12:51:25 +0700

    update corretto jdk(s)

--- Casks/corretto17.rb
+++ Casks/corretto17.rb
@@ -1,7 +1,7 @@
 # https://github.com/corretto/corretto-17
 cask "corretto17" do
-  version "17.0.14.7.1"
-  sha256 "e168900460d2bb6c887d3a9e90918f2d2568d58e4da17cb208318176c804cc1f"
+  version "17.0.16.8.1"
+  sha256 "195d31a1cd695e84bba41ddcf2989a3bc2017484ce89f3f359124ff3cef0dfa8"
 
   url "https://corretto.aws/downloads/resources/#{version.sub(/-\d+/, "")}/amazon-corretto-#{version}-macosx-aarch64.pkg"
   name "AWS Corretto JDK"
@@ -9,11 +9,11 @@ cask "corretto17" do
   homepage "https://github.com/corretto/corretto-17/releases"
 
   livecheck do
-      url "https://corretto.aws/downloads/latest/amazon-corretto-#{version.major}-aarch64-macos-jdk.pkg"
-      strategy :header_match do |headers|
-        headers["location"][%r{/amazon-corretto-(\d+(?:\.\d+)+)-macosx-aarch64\.pkg}i, 1]
-      end
+    url "https://corretto.aws/downloads/latest/amazon-corretto-#{version.major}-aarch64-macos-jdk.pkg"
+    strategy :header_match do |headers|
+      headers["location"][%r{/amazon-corretto-(\d+(?:\.\d+)+)-macosx-aarch64\.pkg}i, 1]
     end
+  end
 
   pkg "amazon-corretto-#{version}-macosx-aarch64.pkg"
 
--- Casks/corretto25.rb
+++ Casks/corretto25.rb
@@ -1,12 +1,12 @@
-# https://github.com/corretto/corretto-21
-cask "corretto21" do
-  version "21.0.8.9.1"
-  sha256 "2352f5eb51bccda7d9b1ff4139df7ae3ec3381ff91d40748f2a35bb5c58f0ff3"
+# https://github.com/corretto/corretto-25/releases
+cask "corretto25" do
+  version "25.0.0.36.1"
+  sha256 "8c07ecbf7f91602c3b41d42c1573f3a7f39fb3699d95d7e96ea46d46e05ae585"
 
   url "https://corretto.aws/downloads/resources/#{version.sub(/-\d+/, "")}/amazon-corretto-#{version}-macosx-aarch64.pkg"
   name "AWS Corretto JDK"
   desc "OpenJDK distribution from Amazon"
-  homepage "https://github.com/corretto/corretto-21/releases"
+  homepage "https://github.com/corretto/corretto-25/releases"
 
   livecheck do
     url "https://corretto.aws/downloads/latest/amazon-corretto-#{version.major}-aarch64-macos-jdk.pkg"
