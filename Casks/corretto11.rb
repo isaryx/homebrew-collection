00d54fe44159ec9d37cc2aa8f1b756d655c0df97
Vu Tran <Vu.Tran2@netapp.com>
2025-09-06 12:44:47 +0700

    update 11

--- Casks/corretto11.rb
+++ Casks/corretto11.rb
@@ -1,7 +1,7 @@
 # https://github.com/corretto/corretto-11/releases
 cask "corretto11" do
-  version "11.0.26.4.1"
-  sha256 "cede642e444e10c50b0834b1dc2388c14e17198caf2a6ee204ee6316d8c506a6"
+  version "11.0.28.6.1"
+  sha256 "1ea8284ecaf5cb98eddcf4f62aed4d5d679026c3e2a924b33178fe2ebe0e71eb"
 
   url "https://corretto.aws/downloads/resources/#{version.sub(/-\d+/, "")}/amazon-corretto-#{version}-macosx-aarch64.pkg"
   name "AWS Corretto JDK"
@@ -9,11 +9,11 @@ cask "corretto11" do
   homepage "https://github.com/corretto/corretto-11/releases"
 
   livecheck do
-      url "https://corretto.aws/downloads/latest/amazon-corretto-#{version.major}-aarch64-macos-jdk.pkg"
-      strategy :header_match do |headers|
-        headers["location"][%r{/amazon-corretto-(\d+(?:\.\d+)+)-macosx-aarch64\.pkg}i, 1]
-      end
+    url "https://corretto.aws/downloads/latest/amazon-corretto-#{version.major}-aarch64-macos-jdk.pkg"
+    strategy :header_match do |headers|
+      headers["location"][%r{/amazon-corretto-(\d+(?:\.\d+)+)-macosx-aarch64\.pkg}i, 1]
     end
+  end
 
   pkg "amazon-corretto-#{version}-macosx-aarch64.pkg"
 
