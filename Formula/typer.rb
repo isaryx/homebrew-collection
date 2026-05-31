9bd6a511b6cdeefffc0afcef7974afd471f655d4
Vu Tran <vu.tran2@netapp.com>
2026-05-31 20:24:17 +0700

    Update Typer formula to version 0.5.4 with new download URLs and SHA256 checksums for all architectures

--- Formula/typer.rb
+++ Formula/typer.rb
@@ -1,26 +1,26 @@
 class Typer < Formula
   desc "Cross-platform typing trainer CLI"
   homepage "https://github.com/isaryx/typer"
-  version "0.5.3"
+  version "0.5.4"
   on_macos do
     on_arm do
-      url "https://github.com/isaryx/typer/releases/download/v0.5.3/typer-0.5.3-darwin-arm64.tar.gz"
-      sha256 "ce3347fd4557844ec00cb13615d46b69e7952673c506aa6e70585ab1b5f808a7"
+      url "https://github.com/isaryx/typer/releases/download/v0.5.4/typer-0.5.4-darwin-arm64.tar.gz"
+      sha256 "d230468e40ccdc72da3e880423c8cedda3a3cb98e693ad384e003a4a3b6f408b"
     end
     on_intel do
-      url "https://github.com/isaryx/typer/releases/download/v0.5.3/typer-0.5.3-darwin-amd64.tar.gz"
-      sha256 "b515e24d719cf37f960a4970f10b520b690e931cb3b2820248f1777145d163cf"
+      url "https://github.com/isaryx/typer/releases/download/v0.5.4/typer-0.5.4-darwin-amd64.tar.gz"
+      sha256 "211c84e3bc150e97b4c91f91a97345e7c171cdfbf8574ed3e690521f5251546d"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/isaryx/typer/releases/download/v0.5.3/typer-0.5.3-linux-arm64.tar.gz"
-      sha256 "c3e10a72daf6bfda14b5ee7d546645253b1cf7934d0aa2e5ffa2a83dd4ec8718"
+      url "https://github.com/isaryx/typer/releases/download/v0.5.4/typer-0.5.4-linux-arm64.tar.gz"
+      sha256 "81dfd28aa7a3aca2e94453aa3a4f336ca106ce513de56cc231a1cecdf10996ee"
     end
     on_intel do
-      url "https://github.com/isaryx/typer/releases/download/v0.5.3/typer-0.5.3-linux-amd64.tar.gz"
-      sha256 "0e046151538ad31c468fd72b7aa4ac8709a8e57087870796686c03eef486951b"
+      url "https://github.com/isaryx/typer/releases/download/v0.5.4/typer-0.5.4-linux-amd64.tar.gz"
+      sha256 "996ee1b5ed957c31bba680c8a5a8a28f097d977a1a6d54f503d6644500134837"
     end
   end
 
@@ -30,6 +30,6 @@ class Typer < Formula
 
   test do
     out = shell_output("#{bin}/typer version")
-    assert_match "0.5.3", out
+    assert_match "0.5.4", out
   end
 end
